95f7e3465bc1660a1efd911aa79beef1c92f4063
aquynh1682 <nm.quynh1608@gmail.com>
2021-07-12 18:18:26 +0700

    reup

--- hoctap/converXML.php
+++ hoctap/converXML.php
@@ -1,5 +1,6 @@
 <?php 
     $conn = mysqli_connect('localhost', 'olirenwm_htht', '123456a@', 'olirenwm_htht');
+    mysqli_set_charset($conn, 'utf8');
     $fire = mysqli_query($conn, "select * from user");
     $xml = new XMLWriter();
     $xml -> openURI("php://output");
